0ac87c31049c4b34122f029891aa2494c94e6f9f
Vlad Negoita <vlad1negoita@gmail.com>
2022-03-25 10:53:51 +0200

    Solved lab4 - ex2
    Signed-off-by: Vlad Negoita <vlad1negoita@gmail.com>

--- laborator/content/toolchain-decompilare/2-warm-up-gotos/bogosort.c
+++ laborator/content/toolchain-decompilare/2-warm-up-gotos/bogosort.c
@@ -36,11 +36,9 @@ int main(void)
 		shuffle(numbers, 6);
 
 		if (is_sorted(numbers, 6))
-
-		/* TODO use goto instead of break */
-		break;
+			goto out;
 	}
-
+	out:
 	for (i = 0; i < 6; i++)
 		printf("%d ", numbers[i]);
 	printf("\n");
