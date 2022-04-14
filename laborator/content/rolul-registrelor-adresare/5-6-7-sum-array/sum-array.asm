efbf7d4f64acf6a53939fe6fd15450034d33e37e
Vlad Negoita <vlad1negoita@gmail.com>
2022-04-14 14:30:10 +0300

    Lab6 - Exercises 5-6-7

--- laborator/content/rolul-registrelor-adresare/5-6-7-sum-array/sum-array.asm
+++ laborator/content/rolul-registrelor-adresare/5-6-7-sum-array/sum-array.asm
@@ -1,12 +1,13 @@
 %include "../utils/printf32.asm"
 
-%define ARRAY_SIZE    10
+%define ARRAY_SIZE 3
 
 section .data
-    byte_array db 8, 19, 12, 3, 6, 200, 128, 19, 78, 102
-    word_array dw 1893, 9773, 892, 891, 3921, 8929, 7299, 720, 2590, 28891
-    dword_array dd 1392849, 12544, 879923, 8799279, 7202277, 971872, 28789292, 17897892, 12988, 8799201
+    byte_array db 8, 19, 12
+    word_array dw 1893, 9773, 892
+    dword_array dd 1392849, 12544, 879923
     print_format db "Array sum is ", 0
+    print_form db "Array sq_sum is ", 0
 
 section .text
 extern printf
@@ -27,8 +28,38 @@ add_byte_array_element:
     PRINTF32 `%s\x0`, print_format
     PRINTF32 `%u\n\x0`, eax
 
+    mov ecx, ARRAY_SIZE
+    xor eax, eax
+add_word_array_element:
+    mov dx, word [word_array +2*(ecx - 1)]
+    add eax, edx
+    loop add_word_array_element ; Decrement ecx, if not zero, add another element.
 
-    ; TODO: Compute sum for elements in word_array and dword_array.
+    PRINTF32 `%s\x0`, print_format
+    PRINTF32 `%u\n\x0`, eax
+
+    mov ecx, ARRAY_SIZE
+    xor eax, eax
+add_dword_array_element:
+    mov edx, dword [dword_array + 4*(ecx - 1)]
+    add eax, edx
+    loop add_dword_array_element ; Decrement ecx, if not zero, add another element.
+
+    PRINTF32 `%s\x0`, print_format
+    PRINTF32 `%u\n\x0`, eax
+
+    xor ebx, ebx
+    mov ecx, ARRAY_SIZE
+add_sq_byte_array_element:
+    xor edx, edx
+    mov dl, byte [byte_array + ecx - 1]
+    mov eax, edx  
+    mul edx
+    add ebx, eax
+    loop add_sq_byte_array_element ; Decrement ecx, if not zero, add another element.
+
+    PRINTF32 `%s\x0`, print_form
+    PRINTF32 `%u\n\x0`, ebx
 
     leave
     ret
