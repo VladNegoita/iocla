2866d8d30d34e73021e7346f42a72d7f41a0ea1e
Vlad Negoita <vlad1negoita@gmail.com>
2022-05-07 18:33:24 +0300

    Lab9 - ex0

--- laborator/content/apel-functii/0-fibo/fibo.asm
+++ laborator/content/apel-functii/0-fibo/fibo.asm
@@ -9,8 +9,23 @@ global main
 main:
     mov ebp, esp
 
+    mov eax, 0
+    mov ebx, 1
+    mov ecx, NUM_FIBO
+    dec ecx
+    push eax
+fibo:
+
+    xor edx, edx
+    add edx, eax
+    add edx, ebx
+    push edx
+    mov ebx, eax
+    mov eax, edx
+    loop fibo
+
     ; TODO - replace below instruction with the algorithm for the Fibonacci sequence
-    sub esp, NUM_FIBO * 4
+    ; sub esp, NUM_FIBO * 4
 
     mov ecx, NUM_FIBO
 print:
