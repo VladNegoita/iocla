4ed42da007fcc17a76877d9795cdec7714eedf89
Vlad Negoita <vlad1negoita@gmail.com>
2022-05-07 18:34:49 +0300

    Solved lab9 - ex4

--- laborator/content/apel-functii/4-5-print-string-len/print-string-len.asm
+++ laborator/content/apel-functii/4-5-print-string-len/print-string-len.asm
@@ -2,6 +2,10 @@
 
 section .data
     mystring db "This is my string", 0
+    reversed times 64 db 0
+    format db "%d", 10, 0
+    format2 db "%s", 10, 0
+    len dd 17
 
 section .text
 
@@ -9,12 +13,36 @@ extern puts
 extern printf
 global main
 
+reverse_string:
+    push ebp
+    mov ebp, esp
+
+    mov eax, dword [ebp+8]
+    mov ecx, dword [ebp+12]
+    add eax, ecx
+    dec eax
+    mov edx, dword [ebp+16]
+
+copy_one_byte:
+    mov bl, byte [eax]
+    mov byte [edx], bl
+    dec eax
+    inc edx
+    loopnz copy_one_byte
+
+    inc edx
+    mov byte [edx], 0
+
+    leave
+    ret
+
 main:
     push ebp
     mov ebp, esp
 
     mov eax, mystring
     xor ecx, ecx
+
 test_one_byte:
     mov bl, byte [eax]
     test bl, bl
@@ -25,6 +53,24 @@ test_one_byte:
 
 out:
     PRINTF32 `[PRINTF32]: %d\n[printf]: \x0`, ecx
+
+    mov [len], ecx
+
+    push ecx
+    push format
+    call printf
+    add esp, 8
+
+    push reversed
+    push dword [len]
+    push mystring
+    call reverse_string
+    add esp, 12
+
+    push reversed
+    push format2
+    call printf
+    add esp, 8
 
     leave
     ret
